0ab00d58824d6a19705494071912e6b159c28ada
Luan.VT <luan.vt@dtsvn.com>
2024-12-09 09:50:51 +0700

    test read json file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({"message": "Hello, Koyeb!"})
+# Route to serve JSON files
+@app.route("/<path:filename>")
+def serve_json(filename):
+    return send_from_directory("static/json", filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
