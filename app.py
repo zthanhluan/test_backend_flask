c812273ee36b9934aa73ed4a641abc7312c24f01
Luan.VT <luan.vt@dtsvn.com>
2024-12-09 14:17:28 +0700

    test

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def convert_video():
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
             },
+            'verbose': True,
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
