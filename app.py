90c0954dc69a75208510faf700dc77a23acb41d4
Luan.VT <luan.vt@dtsvn.com>
2024-12-09 11:34:26 +0700

    update cookies

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def convert_video():
             }],
             'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
             'quiet': True,
+            'cookies': 'www.youtube.com_cookies.txt',
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
